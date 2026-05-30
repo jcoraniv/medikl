8c14eb754a2cea8fb0973f4b748d0e351e16669c
Javier Corani <jcoranivillan@gmail.com>
2026-05-30 19:22:30 -0400

    #1 chore(backend): configure TypeORM async with ConfigService

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        type: 'postgres',
+        host: config.get('DB_HOST'),
+        port: config.get<number>('DB_PORT'),
+        database: config.get('DB_NAME'),
+        username: config.get('DB_USER'),
+        password: config.get('DB_PASS'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        migrations: [__dirname + '/migrations/*{.ts,.js}'],
+        synchronize: false,
+        logging: config.get('NODE_ENV') === 'development',
+      }),
+    }),
+  ],
 })
 export class AppModule {}
